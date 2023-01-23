a55c99073c96ea66ec9607b1270bb468f57113d2
rachael hobbs <=>
2023-01-23 16:55:06 -0500

    clean up project

--- src/Main.kt
+++
@@ -1,3 +0,0 @@
-fun main() {
-    print(sorts.quickSort(listOf(3, 6, 3, 4, 4, 9)).toString())
-}
---
+++ src/main.kt
@@ -0,0 +1,6 @@
+import sorts.quickSort
+
+fun main() {
+    val list = listOf<Int>(3,6,3,4,4,9)
+    print(quickSort(list))
+}
